3a4571852c9e081cb01f63a6344d1d6ced30517f
J.W <jakwings@gmail.com>
2018-04-11 09:29:28 +0800

    v1.0.0-alpha

--- src/main.c
+++ src/main.c
@@ -9,7 +9,7 @@
 #include "vm.h"
 
 #define PROGRAM "ufold"
-#define VERSION "0.0.0"
+#define VERSION "1.0.0-alpha"
 
 #define COPYRIGHT "Copyright (c) 2018 J.W https://github.com/jakwings/ufold"
 #define LICENSE "License: https://opensource.org/licenses/ISC"
