b704352b8e0cd616fddab6dc76bc9a3e621ed519
J.W <jakwings@gmail.com>
2018-04-26 22:50:53 +0800

    Add more tests

--- tests/test.sh
+++ tests/test.sh
@@ -19,19 +19,51 @@ function fold {
     timeout "${seconds}" "${ufold}" "$@"
 }
 
-if [[ -r tmp_flags ]]; then
-    printf 'Retry the previous failed test: ufold %s ... ' "$(cat tmp_flags)"
-    fold $(cat tmp_flags) < tmp_stdin > tmp_stdout 2> tmp_stderr
-
-    exitcode=$?
+function exit_if_failed {
+    local exitcode=$?
     if [[ "${exitcode}" -ne 0 ]]; then
         printf 'Failed\n'
         wc -c tmp_std{in,out}
         cat tmp_stderr
         exit "${exitcode}"
     fi
+}
+
+if [[ -r tmp_flags ]]; then
+    printf 'Retry the previous failed test: ufold %s ... ' "$(cat tmp_flags)"
+    fold $(cat tmp_flags) < tmp_stdin > tmp_stdout 2> tmp_stderr
+    exit_if_failed
     printf 'Done\n'
 fi
+
+# test regular inputs
+i=1
+while read -r flags; do
+    num=$(printf %03d $i)
+    input="fixtures/${num}.in"
+    output="fixtures/${num}.out"
+
+    printf '\r[TEST] ufold %-16s  %s ... ' "${flags}" "${input}"
+
+    printf '%s\n' "${flags}" > tmp_flags
+    # please make sure each input end with a linefeed
+    cat "${input}" "${input}" |
+        tee tmp_stdin |
+            fold $flags > tmp_stdout 2> tmp_stderr
+    exit_if_failed
+
+    cat "${output}" "${output}" > tmp_expect
+    diff -u tmp_expect tmp_stdout > tmp_diff 2>&1
+    if [[ "$?" -ne 0 ]]; then
+        printf 'Failed\n'
+        cat tmp_diff
+        exit 2
+    fi
+
+    printf 'Done\n'
+
+    i=$(( i + 1 ))
+done < flags.txt
 
 # test exit status
 flags=(
@@ -56,14 +88,7 @@ while [[ $i -lt ${#flags[@]} ]]; do
         head -c10000 /dev/random |
             tee tmp_stdin |
                 fold $args > tmp_stdout 2> tmp_stderr
-
-        exitcode=$?
-        if [[ "${exitcode}" -ne 0 ]]; then
-            printf 'Failed\n'
-            wc -c tmp_std{in,out}
-            cat tmp_stderr
-            exit "${exitcode}"
-        fi
+        exit_if_failed
 
         j=$(( j + 1 ))
     done
@@ -71,4 +96,5 @@ while [[ $i -lt ${#flags[@]} ]]; do
 
     i=$(( i + 1 ))
 done
-rm tmp_flags tmp_std{in,out,err}
+
+rm -f tmp_*
