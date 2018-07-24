a19e9e67ef1909b3ad6ac1c20e0ac283c51f11c0
J.W <jakwings@gmail.com>
2018-07-25 00:17:36 +0800

    v1.0.0-eta
    [ci skip]

--- src/main.c
+++ src/main.c
@@ -9,7 +9,7 @@
 #include "vm.h"
 
 #define PROGRAM "ufold"
-#define VERSION "1.0.0-zeta"
+#define VERSION "1.0.0-eta"
 
 #define COPYRIGHT "Copyright (c) 2018 J.W https://github.com/jakwings/ufold"
 #define LICENSE "License: https://opensource.org/licenses/ISC"
